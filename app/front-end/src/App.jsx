cb7fdbf742a5a5d7e5ad8f0ee0d1b0777110cd33
Abhiram <a4abhiram125@gmail.com>
2023-06-07 23:05:55 +0530

    updated navigate to auth/signin when user starts the app

--- app/front-end/src/App.jsx
+++ app/front-end/src/App.jsx
@@ -1,5 +1,10 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Navigate,
+  Route,
+  Routes,
+} from "react-router-dom";
 import AuthPageLayout from "./components/authentication/auth-page-layout";
 
 function App() {
@@ -7,6 +12,7 @@ function App() {
     <div className="App">
       <Router>
         <Routes>
+          <Route path="/" element={<Navigate to="/auth/signin" replace />} />
           <Route path="/auth/*" element={<AuthPageLayout />} />
         </Routes>
       </Router>
