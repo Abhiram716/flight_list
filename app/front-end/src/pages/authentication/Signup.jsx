91267d726ac840a7ec50f7b1e5d2d9c89fee6b69
Abhiram Kantipudi <a4abhiram125@gmail.com>
2023-09-12 23:59:54 +0530

    perf:integrated formik with signup form

--- app/front-end/src/pages/authentication/Signup.jsx
+++ app/front-end/src/pages/authentication/Signup.jsx
@@ -1,44 +1,67 @@
 import React from 'react';
 import { Box, TextField, InputAdornment, Button } from '@mui/material';
 import { FaUserAlt, FaLock } from 'react-icons/fa';
+import { useFormik } from 'formik';
 
 const Signup = () => {
+  const formik = useFormik({
+    initialValues: {
+      username: '',
+      password: '',
+    },
+    onSubmit: (values) => {
+      //submit logic
+    },
+  });
+
   return (
-    <Box
-      sx={{
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'center',
-        boxSizing: 'border-box',
-        m: 5,
-      }}
-    >
-      <TextField
-        label="username"
-        margin="dense"
-        InputProps={{
-          endAdornment: (
-            <InputAdornment position="end">
-              <FaUserAlt />
-            </InputAdornment>
-          ),
+    <form onSubmit={formik.handleSubmit}>
+      <Box
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          boxSizing: 'border-box',
+          m: 5,
         }}
-      />
-      <TextField
-        label="password"
-        margin="dense"
-        InputProps={{
-          endAdornment: (
-            <InputAdornment position="end">
-              <FaLock />
-            </InputAdornment>
-          ),
-        }}
-      />
-      <Button variant="contained" type="submit" sx={{ mt: 2 }}>
-        Submit
-      </Button>
-    </Box>
+      >
+        <TextField
+          id="username"
+          name="username"
+          label="username"
+          margin="dense"
+          value={formik.values.username}
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          InputProps={{
+            endAdornment: (
+              <InputAdornment position="end">
+                <FaUserAlt />
+              </InputAdornment>
+            ),
+          }}
+        />
+        <TextField
+          id="password"
+          name="password"
+          label="password"
+          margin="dense"
+          value={formik.values.password}
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          InputProps={{
+            endAdornment: (
+              <InputAdornment position="end">
+                <FaLock />
+              </InputAdornment>
+            ),
+          }}
+        />
+        <Button variant="contained" type="submit" sx={{ mt: 2 }}>
+          Submit
+        </Button>
+      </Box>
+    </form>
   );
 };
 
