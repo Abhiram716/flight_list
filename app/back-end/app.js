fb0b92e08dd89d5c55eaa8398ca53ddb11a3838d
Abhiram Kantipudi <a4abhiram125@gmail.com>
2023-09-14 00:31:44 +0530

    perf: added cors policy to the remote resource

--- app/back-end/app.js
+++ app/back-end/app.js
@@ -33,7 +33,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(
   cors({
-    origin: 'http://localhost:3000',
+    origin: ['http://localhost:3000', 'https://flights-list-api.onrender.com'],
   }),
 );
 app.use('/flights', router);
