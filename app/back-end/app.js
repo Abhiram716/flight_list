7a3bee2fa0d640aa973f28842214e6692baeb0d1
Abhiram Kantipudi <a4abhiram125@gmail.com>
2023-09-17 08:15:10 +0530

    update cors origin

--- app/back-end/app.js
+++ app/back-end/app.js
@@ -33,10 +33,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(
   cors({
-    origin: [
-      'http://localhost:3000',
-      'https://bright-travesseiro-4439e1.netlify.app',
-    ],
+    origin: ['http://localhost:3000', 'https://flight-list-client.netlify.app'],
   }),
 );
 app.use('/flights', router);
